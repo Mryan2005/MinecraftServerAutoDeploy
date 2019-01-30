e94782030d6ea43fbdbde9f008a9ef1a330b8744
Mr.yan <40586009+Mryan2005@users.noreply.github.com>
2019-01-30 21:54:41 +0800

    update

---
+++ src/push/main.py
@@ -0,0 +1,3 @@
+import subprocess
+child = subprocess.Popen(r'H:\MinecraftServerAutoDeploy\src\push\updat.sh',shell=True)
+child.wait()
--- updat.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cur_dateTime="`date +%Y-%m-%d,%H:%m:%s`"
-git add .
-git commit -m 'update ${cur_dateTime}'
-git push origin
