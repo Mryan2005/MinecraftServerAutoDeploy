1a9e4d87d1a8a738ea4357a834ca403ce297fd8c
Mr.yan <40586009+Mryan2005@users.noreply.github.com>
2019-01-30 22:49:49 +0800

    update

--- src/push/push.sh
+++ src/push/push.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+source ./setting.config
+server_name=$server_name
 cur_dateTime="`date +%Y-%m-%d,%H:%m:%s`"
 cd '/root'
+if [[ ! -d "$server_name" ]]; then
+	echo "文件夹不存在"
+else
+	echo "文件夹存在"
+fi
 git add .
 git commit -m 'update'
 git push origin
